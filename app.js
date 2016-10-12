7c78895936c3565a495a944805e652fcb5c5cc49
Hayden Carlson <haydog2@hotmail.com>
2016-10-12 19:45:39 +0000

    Added delete url

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-var express = require('express');
-var app = express();
-var PORT = 8080;
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = app.set('port', 8080);
+const methodOverride = require('method-override');
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded());
+
+
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
+app.use(methodOverride("_method"));
 
+
+
+//where to create new urls
+//CREATE - C
 app.get("/", (req, res) => {
   res.render("urls_new");
 });
 
+//add url to database 
 app.post("/urls", (req, res) => {
   // var parsedObj = JSON.parse(req.body;
-  
   var shortURL = generateRandomString(req.body.longURL);
   console.log(shortURL);
   console.log(req.body.longURL);  
@@ -33,7 +37,13 @@ app.post("/urls", (req, res) => {
   res.redirect(302, `urls/${shortURL}`)  
    // Respond with 'Ok' (we will replace this)
 });
+//RETRIEVE - R
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
+//show url by id
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   console.log(templateVars);
@@ -46,6 +56,7 @@ app.get("/error", (req, res) => {
 
 });
 
+//redirect to id if id is valid in database
 app.get("/:id", (req, res) => {
   let longURL = urlDatabase[req.params.id];
   if (urlDatabase[req.params.id] === undefined) {
@@ -56,10 +67,18 @@ app.get("/:id", (req, res) => {
 });
 
 
+//DELETE -D
+app.delete("/urls/:shortURL", (req, res) => {
+  if (urlDatabase.hasOwnProperty(req.params.shortURL)) {
+    delete urlDatabase[req.params.shortURL];
+  }
+  res.redirect(302, "/urls");
+  
+});
 
 
-app.listen(PORT, () => {
-  console.log(`Express server is now running on port: ${PORT}`);
+app.listen(app.get('port'), () => {
+  console.log(`Express server is now running on port: ${app.get('port')}`);
 
 });
 
