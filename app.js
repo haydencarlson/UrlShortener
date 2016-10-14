5b2319a312004d4057e3fc321535d1d0be72d804
Hayden Carlson <haydog2@hotmail.com>
2016-10-14 21:13:45 +0000

    All

--- app.js
+++ app.js
@@ -5,44 +5,31 @@ const PORT = app.set('port', 8080);
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 
-
-
-
-
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  "b2xVn2": {originalURL: "http://www.lighthouselabs.ca", userID: 'sdf'},
   "9sm5xK": "http://www.google.com"
 };
-
 var users = {};
-
-
-
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(methodOverride("_method"));
 app.use(cookieParser());
 // app.use(Cookies());
 
-
-
-
 //where to create new urls
 //CREATE - C
 app.get("/", (req, res) => {
-//fix
- // let username = users.email
-
- console.log("home");
+  //fix
+  // let username = users.emai
   res.render("urls_new");
-  
 
 });
 
 app.get("/register", (req, res) => {
-
   res.redirect("/");
 });
 
@@ -51,20 +38,20 @@ app.post("/urls", (req, res) => {
   // var parsedObj = JSON.parse(req.body;
   var shortURL = generateRandomString(req.body.longURL);
   console.log(shortURL);
-  console.log(req.body.longURL);  
+  console.log(req.body.longURL);
   urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase);// debug statement to see POST parameters
-  res.redirect(302, `urls/${shortURL}`)  
-   // Respond with 'Ok' (we will replace this)
+  urlDatabase.userID = req.cookies.user_id;
+  console.log(urlDatabase); // debug statement to see POST parameters
+  res.redirect(302, `urls/${shortURL}`)
+    // Respond with 'Ok' (we will replace this)
 });
 
 app.post("/register", (req, res) => {
 
   const randomuserid = generateRandomID();
 
- 
-  for (let userID in users){
 
+  for (let userID in users) {
     let user = users[userID]
 
     if (user.email === req.body.email) {
@@ -73,16 +60,16 @@ app.post("/register", (req, res) => {
   }
 
   if (req.body.email === "" || undefined) {
-
-   res.redirect('error');
+    res.redirect('error');
 
   } else if (req.body.password === "" || undefined) {
-
-   res.redirect('error');
+    res.redirect('error');
   }
 
   res.cookie("user_id", randomuserid);
   res.cookie("email", req.body.email)
+
+
 
   const user = {
     id: randomuserid,
@@ -91,7 +78,7 @@ app.post("/register", (req, res) => {
   }
 
 
-  users[randomuserid] = user;  
+  users[randomuserid] = user;
   res.redirect("login");
 
 });
@@ -102,43 +89,44 @@ app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/");
 });
- 
+
 app.post("/login", (req, res) => {
 
-console.log("this works");
-  for (let userID in users){
-console.log("this works 1");
+  console.log("this works");
+  for (let userID in users) {
+    console.log("this works 1");
     let user = users[userID]
 
-    if (user.email === req.body.email && user.password === req.body.password ) {
-  
+    if (user.email === req.body.email && user.password === req.body.password) {
+
       console.log("checking id");
-     res.redirect("/", username);
+      res.redirect("/");
 
     } else {
-    res.redirect("/register");
+      res.redirect("/register");
     }
   }
-   
-  
 });
 
 app.get("/login", (req, res) => {
 
-res.redirect("/");
+  res.redirect("/");
 });
-
-
 
 //RETRIEVE - R
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 //show url by id
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
   console.log(templateVars);
   res.render("urls_show", templateVars);
 });
@@ -147,7 +135,7 @@ app.get("/error", (req, res) => {
   res.render("error");
 
 });
-app.get('/users', (req, res) => res.json(users))
+app.get('/users', (req, res) => res.json(urlDatabase))
 
 //redirect to id if id is valid in database
 app.get("/:id", (req, res) => {
@@ -172,7 +160,7 @@ app.delete("/urls/:shortURL", (req, res) => {
     delete urlDatabase[req.params.shortURL];
   }
   res.redirect(302, "/urls");
-  
+
 });
 
 
@@ -186,17 +174,16 @@ const generateRandomString = () => {
   let randomString = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( let i=0; i < 6; i++ )
-        randomString += possible.charAt(Math.floor(Math.random() * possible.length));
-  return  randomString;
+  for (let i = 0; i < 6; i++)
+    randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+  return randomString;
 };
 
 const generateRandomID = () => {
   let randomID = "";
   var possible = "0123456789";
 
-    for( let i=0; i < 5; i++ )
-        randomID += possible.charAt(Math.floor(Math.random() * possible.length));
-  return  randomID;
+  for (let i = 0; i < 5; i++)
+    randomID += possible.charAt(Math.floor(Math.random() * possible.length));
+  return randomID;
 };
-
